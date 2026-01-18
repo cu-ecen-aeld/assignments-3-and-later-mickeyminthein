4e26d1e3b85575fabf6e25d1fc23a1187374cc3d
Mickey Thein <mickeyminthein@gmail.com>
2026-01-18 18:44:03 -0500

    added header files

--- examples/systemcalls/systemcalls.h
+++ examples/systemcalls/systemcalls.h
@@ -1,6 +1,11 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdarg.h>
+#include <fcntl.h>      // For open() and O_* flags
 
 bool do_system(const char *command);
 
