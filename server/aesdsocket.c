9f1b768e93a8170874b3f8bced17d3ed169f79c7
Mickey Thein <mickeyminthein@gmail.com>
2026-03-07 15:56:23 -0500

    added double fork for handling in daemon mode

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -1,224 +1,215 @@
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <errno.h>
-#include <arpa/inet.h>
-#include <netinet/in.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <syslog.h>
 #include <signal.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <sys/wait.h>
+#include <syslog.h>
+#include <fcntl.h>
 
-#define PORT 9000
+#define PORT "9000"
+#define BACKLOG 10
 #define DATA_FILE "/var/tmp/aesdsocketdata"
-#define BACKLOG 5
-#define RECV_BUFFER 1024
+#define BUFFER 1024
 
-static volatile sig_atomic_t exit_requested = 0;
+volatile sig_atomic_t exit_requested = 0;
 
 void signal_handler(int signo)
 {
-    syslog(LOG_INFO, "Caught signal, exiting");
     exit_requested = 1;
 }
 
-int main(int argc, char *argv[])
+void sigchld_handler(int s)
 {
-    int daemon_mode = 0;
-
-if (argc == 2 && strcmp(argv[1], "-d") == 0) {
-    daemon_mode = 1;
+    while(waitpid(-1, NULL, WNOHANG) > 0);
 }
-    int server_fd = -1;
-    int client_fd = -1;
-    struct sockaddr_in server_addr, client_addr;
-    socklen_t client_addr_len;
-    char recv_buffer[RECV_BUFFER];
 
-    openlog("aesdsocket", LOG_PID, LOG_USER);
+void *get_in_addr(struct sockaddr *sa)
+{
+    if (sa->sa_family == AF_INET)
+        return &(((struct sockaddr_in*)sa)->sin_addr);
 
-    // Register signal handlers
-    signal(SIGINT, signal_handler);
-    signal(SIGTERM, signal_handler);
+    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
 
-    // Create socket
-    server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_fd == -1) {
-        syslog(LOG_ERR, "Socket creation failed: %s", strerror(errno));
-        return -1;
-    }
+void handle_client(int client_fd)
+{
+    char buffer[BUFFER];
+    char *packet = NULL;
+    size_t packet_size = 0;
+    ssize_t bytes;
 
-    int optval = 1;
-    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval));
-
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(PORT);
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-
-    // Bind
-    if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
-        syslog(LOG_ERR, "Bind failed: %s", strerror(errno));
-        close(server_fd);
-        return -1;
-    }
-    printf("aesdsocket binding successful!.");
-
-    //Run as Daemon
-    if (daemon_mode) {
-        pid_t pid = fork();
-
-        if (pid < 0) {
-            syslog(LOG_ERR, "Fork failed");
-            return -1;
+    while((bytes = recv(client_fd, buffer, BUFFER, 0)) > 0)
+    {
+        char *tmp = realloc(packet, packet_size + bytes);
+        if(!tmp)
+        {
+            syslog(LOG_ERR,"realloc failed");
+            free(packet);
+            return;
         }
 
-        if (pid > 0) {
-            // Parent exits
+        packet = tmp;
+        memcpy(packet + packet_size, buffer, bytes);
+        packet_size += bytes;
+
+        if(memchr(buffer,'\n',bytes))
+            break;
+    }
+
+    if(bytes == -1)
+    {
+        syslog(LOG_ERR,"recv error");
+        free(packet);
+        return;
+    }
+
+    int fd = open(DATA_FILE,O_CREAT|O_WRONLY|O_APPEND,0644);
+    if(fd == -1)
+    {
+        syslog(LOG_ERR,"file open failed");
+        free(packet);
+        return;
+    }
+
+    write(fd,packet,packet_size);
+    close(fd);
+    free(packet);
+
+    fd = open(DATA_FILE,O_RDONLY);
+    if(fd == -1)
+        return;
+
+    while((bytes = read(fd,buffer,BUFFER)) > 0)
+        send(client_fd,buffer,bytes,0);
+
+    close(fd);
+}
+
+int main(int argc,char *argv[])
+{
+    int sockfd,new_fd;
+    struct addrinfo hints,*servinfo,*p;
+    struct sockaddr_storage their_addr;
+    socklen_t sin_size;
+    int yes=1;
+    char s[INET6_ADDRSTRLEN];
+
+    int daemon_mode = 0;
+    if(argc == 2 && strcmp(argv[1],"-d")==0)
+        daemon_mode = 1;
+
+    openlog("aesdsocket",0,LOG_USER);
+
+    struct sigaction sa_term;
+    sa_term.sa_handler = signal_handler;
+    sigemptyset(&sa_term.sa_mask);
+    sa_term.sa_flags = 0;
+    sigaction(SIGTERM,&sa_term,NULL);
+    sigaction(SIGINT,&sa_term,NULL);
+
+    struct sigaction sa_child;
+    sa_child.sa_handler = sigchld_handler;
+    sigemptyset(&sa_child.sa_mask);
+    sa_child.sa_flags = SA_RESTART;
+    sigaction(SIGCHLD,&sa_child,NULL);
+
+    memset(&hints,0,sizeof hints);
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+
+    getaddrinfo(NULL,PORT,&hints,&servinfo);
+
+    for(p=servinfo;p!=NULL;p=p->ai_next)
+    {
+        sockfd = socket(p->ai_family,p->ai_socktype,p->ai_protocol);
+        if(sockfd == -1)
+            continue;
+
+        setsockopt(sockfd,SOL_SOCKET,SO_REUSEADDR,&yes,sizeof yes);
+
+        if(bind(sockfd,p->ai_addr,p->ai_addrlen)==-1)
+        {
+            close(sockfd);
+            continue;
+        }
+
+        break;
+    }
+
+    freeaddrinfo(servinfo);
+
+    if(p==NULL)
+    {
+        syslog(LOG_ERR,"bind failed");
+        exit(1);
+    }
+
+    if(listen(sockfd,BACKLOG)==-1)
+    {
+        syslog(LOG_ERR,"listen failed");
+        exit(1);
+    }
+
+    if(daemon_mode)
+    {
+        pid_t pid = fork();
+        if(pid>0)
+            exit(EXIT_SUCCESS);
+
+        setsid();
+        chdir("/");
+        close(STDIN_FILENO);
+        close(STDOUT_FILENO);
+        close(STDERR_FILENO);
+    }
+
+    while(!exit_requested)
+    {
+        sin_size = sizeof their_addr;
+
+        new_fd = accept(sockfd,(struct sockaddr*)&their_addr,&sin_size);
+
+        if(new_fd == -1)
+        {
+            if(errno == EINTR && exit_requested)
+                break;
+
+            continue;
+        }
+
+        inet_ntop(their_addr.ss_family,
+                  get_in_addr((struct sockaddr*)&their_addr),
+                  s,sizeof s);
+
+        syslog(LOG_INFO,"Accepted connection from %s",s);
+
+        pid_t pid = fork();
+
+        if(pid==0)
+        {
+            close(sockfd);
+            handle_client(new_fd);
+            close(new_fd);
             exit(0);
         }
 
-        // Child continues
-
-        // Create new session
-        if (setsid() < 0) {
-            syslog(LOG_ERR, "setsid failed");
-            return -1;
-        }
-
-        // Change working directory
-        if (chdir("/") < 0) {
-            syslog(LOG_ERR, "chdir failed");
-            return -1;
-        }
-
-        // Redirect standard file descriptors to /dev/null
-        int devnull = open("/dev/null", O_RDWR);
-        if (devnull >= 0) {
-            dup2(devnull, STDIN_FILENO);
-            dup2(devnull, STDOUT_FILENO);
-            dup2(devnull, STDERR_FILENO);
-            if (devnull > 2) {
-                close(devnull);
-            }
-        }
+        close(new_fd);
     }
 
-    // Listen
-    if (listen(server_fd, BACKLOG) == -1) {
-        syslog(LOG_ERR, "Listen failed: %s", strerror(errno));
-        close(server_fd);
-        return -1;
-    }
+    close(sockfd);
+    remove(DATA_FILE);
 
-    puts("Waiting for connections ...");
-
-    while (!exit_requested) {
-
-        client_addr_len = sizeof(client_addr);
-        client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len);
-
-        if (client_fd == -1) {
-            if (exit_requested)
-                break;
-            syslog(LOG_ERR, "Accept failed: %s", strerror(errno));
-            close(server_fd);
-            return -1;
-        }
-
-        char client_ip[INET_ADDRSTRLEN];
-        inet_ntop(AF_INET, &client_addr.sin_addr, client_ip, INET_ADDRSTRLEN);
-        syslog(LOG_INFO, "Accepted connection from %s", client_ip);
-
-        char *packet_buffer = NULL;
-        size_t packet_size = 0;
-        ssize_t bytes_received;
-
-        // Receive until newline
-        while ((bytes_received = recv(client_fd, recv_buffer, RECV_BUFFER, 0)) > 0) {
-
-            char *temp = realloc(packet_buffer, packet_size + bytes_received);
-            if (!temp) {
-                syslog(LOG_ERR, "Memory allocation failed");
-                free(packet_buffer);
-                close(client_fd);
-                close(server_fd);
-                return -1;
-            }
-
-            packet_buffer = temp;
-            memcpy(packet_buffer + packet_size, recv_buffer, bytes_received);
-            packet_size += bytes_received;
-
-            if (memchr(packet_buffer, '\n', packet_size)) {
-                break;
-            }
-        }
-
-        if (bytes_received == -1) {
-            syslog(LOG_ERR, "Receive failed: %s", strerror(errno));
-            free(packet_buffer);
-            close(client_fd);
-            close(server_fd);
-            return -1;
-        }
-
-        // Append packet to file
-        if (packet_buffer) {
-            int file_fd = open(DATA_FILE, O_CREAT | O_WRONLY | O_APPEND, 0644);
-            if (file_fd == -1) {
-                syslog(LOG_ERR, "File open failed: %s", strerror(errno));
-                free(packet_buffer);
-                close(client_fd);
-                close(server_fd);
-                return -1;
-            }
-
-            if (write(file_fd, packet_buffer, packet_size) == -1) {
-                syslog(LOG_ERR, "File write failed: %s", strerror(errno));
-                free(packet_buffer);
-                close(file_fd);
-                close(client_fd);
-                close(server_fd);
-                return -1;
-            }
-
-            close(file_fd);
-            free(packet_buffer);
-        }
-
-        // Send entire file back to client
-        int file_fd = open(DATA_FILE, O_RDONLY);
-        if (file_fd == -1) {
-            syslog(LOG_ERR, "File reopen failed: %s", strerror(errno));
-            close(client_fd);
-            close(server_fd);
-            return -1;
-        }
-
-        while ((bytes_received = read(file_fd, recv_buffer, RECV_BUFFER)) > 0) {
-            if (send(client_fd, recv_buffer, bytes_received, 0) == -1) {
-                syslog(LOG_ERR, "Send failed: %s", strerror(errno));
-                close(file_fd);
-                close(client_fd);
-                close(server_fd);
-                return -1;
-            }
-        }
-
-        close(file_fd);
-
-        syslog(LOG_INFO, "Closed connection from %s", client_ip);
-
-        close(client_fd);
-    }
-
-    // Graceful shutdown
-    close(server_fd);
-    unlink(DATA_FILE);
+    syslog(LOG_INFO,"Server shutting down");
     closelog();
 
     return 0;
