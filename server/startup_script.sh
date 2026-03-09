25d9fe4a94acfbde8f998cf8e7ca62e9d85f4341
Mickey Thein <mickeyminthein@gmail.com>
2026-03-08 20:27:05 -0400

    modified startup script to work on qemu

--- server/startup_script.sh
+++ server/startup_script.sh
@@ -1,25 +1,18 @@
 #!/bin/sh
 ### BEGIN INIT INFO
-# Provides:          aesdsocket
-# Required-Start:    $network
-# Required-Stop:     $network
-# Default-Start:     2 3 4 5
-# Default-Stop:      0 1 6
-# Short-Description: aesdsocket daemon
+# aesdsocket start stop script 
+# Author: Min Min Thein
 ### END INIT INFO
-
-PIDFILE=/var/run/aesdsocket.pid
-DAEMON=/usr/bin/aesdsocket
 
 case "$1" in
     start)
         echo "Starting aesdsocket"
         [ -d /var/run ] || mkdir -p /var/run
-        start-stop-daemon -S -n aesdsocket -a $DAEMON -p $PIDFILE -- -d
+        start-stop-daemon -S -n aesdsocket -a /usr/bin/aesdsocket -- -d
         ;;
     stop)
         echo "Stopping aesdsocket"
-        start-stop-daemon -K -n aesdsocket -p $PIDFILE
+        start-stop-daemon -K -n aesdsocket 
         ;;
     *)
         echo "Usage: $0 {start|stop}"
